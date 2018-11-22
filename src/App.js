424b73cf90ba7d030de21395cd41dd41f88bad5b
Jakub <jakub.rozi.rozak@gmail.com>
2018-11-22 18:18:23 +0000

    adding jsonp functionality and hyperlings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import axios from 'axios'
+
+import fetchJsonp from 'fetch-jsonp'
 
 class App extends Component {
-state ={
-    data:null
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        data : null
+    };
+    
+    
 }
 componentDidMount(){
-axios.get('https://api.idio.co/1.0/content?key=URN4QXKCG3QD3Y5MS51A')
-  .then(res =>{
-    const final = res.data.substr(5).slice(0,-6)
-    let parsed = JSON.parse(final);
-    this.setState({
-      data:parsed.content
+
+
+   fetchJsonp('https://api.idio.co/1.0/content?key=URN4QXKCG3QD3Y5MS51A&callback=myCallback',{
+    jsonpCallbackFunction:'myCallback'
+   })
+    .then((response)=>{
+      return response.json()
+   }).then((json)=>{
+     console.log(json)
+     var res = json
+     this.setState({
+      data:res.content
     })
-    })
+  }).catch(function(ex) {
+  console.log('parsing failed', ex)
+})
+
+
+
 
 }
+
 
 handleMouseOver = (event) => {
   event.target.className="txt2"
@@ -30,12 +49,13 @@ handleMouseLeave = (event) => {
   render() {
     const content = this.state.data
     
+     
     const display = (this.state.data !== null)?(
        content.map((e)=>{
           return (
             <div className="card">
-            <img src={'http://'+e.main_image_url.substr(2)} className="pic" />
-            <h1 className="txt"id="text" key={e.id} onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>{e.title}</h1>
+            <a href={e.original_url}><img src={'http://'+e.main_image_url.substr(2)} className="pic" /></a>
+            <a className="anchor" href={e.original_url}><h1 className="txt"id="text" key={e.id} onMouseOver={this.handleMouseOver} onMouseLeave={this.handleMouseLeave}>{e.title}</h1></a>
             </div>
           )
        })
